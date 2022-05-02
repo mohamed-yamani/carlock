3a96270f74996513dd2f0e4ff435a948c961d1fe
fail2ban1337 <ayoubebelomari@gmail.com>
2022-05-02 19:36:49 +0000

    flutter_google_maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBf8-cic241rojL1Fl3eCSqXXFjAIkQoZ0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/presentation/map/map.dart
+++ lib/presentation/map/map.dart
@@ -1,46 +1,104 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
+
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
 class MapSampleState extends State<MapSample> {
+  GoogleMapController? _controller;
+
+  void _onMapCreated(GoogleMapController controller) {
+    // ! check if _controller is null assign it to controller
+    _controller ??= controller;
+    setState(() {
+      _markers.add(const Marker(
+        markerId: MarkerId(
+          'marker_id_1',
+        ),
+        position: LatLng(32.8821743, -6.897816),
+        infoWindow: InfoWindow(
+          title: 'Marker 1',
+          snippet: 'This is a marker',
+        ),
+      ));
+    });
+  }
+
+  final _markers = <Marker>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(32.88103, -6.9062382),
-          zoom: 13.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
-            attributionBuilder: (_) {
-              return const Text(
-                "1337 future is loading   ",
-                style: TextStyle(color: Colors.red),
-              );
-            },
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(32.8821743, -6.897816),
+              zoom: 16,
+            ),
+            onMapCreated: _onMapCreated,
+            markers: Set<Marker>.of(_markers),
           ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 80.0,
-                height: 80.0,
-                point: LatLng(32.88103, -6.9062382),
-                builder: (ctx) =>
-                    const Icon(Icons.location_on, color: Colors.red),
-              ),
-            ],
-          ),
+          const ReturnButton(),
+          const ButtonText(),
         ],
+      ),
+    );
+  }
+}
+
+class ButtonText extends StatelessWidget {
+  const ButtonText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: const Padding(
+        padding: EdgeInsets.only(bottom: 8.0),
+        child: Text(
+          '1337 future is loading',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
+
+class ReturnButton extends StatelessWidget {
+  const ReturnButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topLeft,
+      margin: const EdgeInsets.only(top: 40, left: 10),
+      height: 45.0,
+      width: 45.0,
+      child: FittedBox(
+        child: FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor.withOpacity(0.9),
+          elevation: 0,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Icon(
+              Icons.arrow_back_ios,
+            ),
+          ),
+        ),
       ),
     );
   }
